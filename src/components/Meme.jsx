b8b9e836172bf848d79a27a23cb68201b3051436
Denis Homoki <homoki.denisth@gmail.com>
2022-07-28 15:16:59 +0300

    added top text and bottom text

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Meme = () => {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    testImg:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoyL9Frn6VLTgjkfge6OG_MLqVQPxQZarI8g&usqp=CAU",
+  });
+
+  const memeInput = (e) => {
+    const { value, name } = e.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
+  };
+
   return (
-    <div>
-      <h1>This is the meme section</h1>
+    <div className="meme">
+      <form>
+        <div className="inputs">
+          <input
+            type="text"
+            value={meme.topText}
+            name="topText"
+            onChange={memeInput}
+          />
+          <input
+            type="text"
+            value={meme.bottomText}
+            name="bottomText"
+            onChange={memeInput}
+          />
+        </div>
+        <button>Get A New Meme Image 🖼</button>
+        <div className="meme-image">
+          <img src={meme.testImg} alt="meme" />
+          <h1 className="top-text">{meme.topText}</h1>
+          <h1 className="bottom-text">{meme.bottomText}</h1>
+        </div>
+      </form>
     </div>
   );
 };
